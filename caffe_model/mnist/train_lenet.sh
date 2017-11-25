21305c76ac0090fe1c4c6ce75625976c9e9d708b
alvin <alvinsunyixiao@gmail.com>
2017-11-25 17:42:08 -0600

    update training script

--- caffe_model/mnist/train_lenet.sh
+++ caffe_model/mnist/train_lenet.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 set -e
 
-../../caffe/build/tools/caffe train --solver=./lenet_solver.prototxt $@
+export PYTHONPATH=$(pwd -P)/pylayer:$PYTHONPATH
+mkdir -p snapshot
+../../caffe/build/tools/caffe train --solver=./lenet_solver.prototxt 
