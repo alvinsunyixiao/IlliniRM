000604df27f5406c2c268cee0aff07acd7d62382
alvin <alvinsunyixiao@gmail.com>
2017-11-29 23:02:03 -0600

    update mnist model zoo

--- training-zoo/mnist/pylayer/mnist_aug.py
+++ training-zoo/mnist/pylayer/mnist_aug.py
@@ -4,11 +4,11 @@ import cv2
 from scipy.ndimage.interpolation import shift
 
 
-def invert(img):
-    if np.random.rand() < 0.5:
-        return img
-    else:
+def invert(img, prob = 0.5):
+    if np.random.rand() < prob:
         return 1 - img
+    else:
+        return img
 
 def scale(img, rng):
     assert len(rng) == 2, "range value has exactly two boundary numbers"
@@ -65,13 +65,22 @@ def addnoise(img, max_std):
     noise = np.random.normal(0, std, img.shape)
     return img + noise
 
-def doAugmentation(img):
-    img = rotate(img, [-15,15])
-    img, sc = scale(img, [0.6,1.4])
-    img = translate(img, [-3,3], sc)
-    img = invert(img)
-    img = contrast(img, 0.6)
-    img = addnoise(img, 0.08)
+def doAugmentation(img,
+                   min_rot = -15,
+                   max_rot = 15,
+                   min_scale = 0.6,
+                   max_scale = 1.4,
+                   min_translate = -3,
+                   max_translate = 3,
+                   invert_prob = 0.5,
+                   max_contrast = 0.6,
+                   max_noise = 0.08):
+    img = rotate(img, [min_rot, max_rot])
+    img, sc = scale(img, [min_scale, max_scale])
+    img = translate(img, [min_translate, max_translate], sc)
+    img = invert(img, invert_prob)
+    img = contrast(img, max_contrast)
+    img = addnoise(img, max_noise)
     return img
 
 class MnistAugmentationLayer(caffe.Layer):
@@ -82,12 +91,22 @@ class MnistAugmentationLayer(caffe.Layer):
 
     def reshape(self, bottom, top):
         top[0].reshape(*bottom[0].data.shape)
+        self.params = eval(self.param_str)
 
     def forward(self, bottom, top):
         top[0].data[...] = bottom[0].data[...]
         for idx in range(top[0].data.shape[0]):
             im = top[0].data[idx,0,:,:]
-            top[0].data[idx,0,:,:] = doAugmentation(im)
+            top[0].data[idx,0,:,:] = doAugmentation(im,
+                                                    self.params['min_rotation'],
+                                                    self.params['max_rotation'],
+                                                    self.params['min_scale'],
+                                                    self.params['max_scale'],
+                                                    self.params['min_translation'],
+                                                    self.params['max_translation'],
+                                                    self.params['invert_probability'],
+                                                    self.params['max_contrast'],
+                                                    self.params['max_noise'])
 
     def backward(self, top, propagate_down, bottom):
         pass
