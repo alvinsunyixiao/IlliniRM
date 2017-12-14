1c0d6def38deb1c6cfb3bd6bcfd7139f992c380b
Roger Q <twofs0.5@gmail.com>
2017-12-14 10:51:50 -0600

    minor improvement

--- roger_detection_test/num_recog.py
+++ roger_detection_test/num_recog.py
@@ -41,20 +41,23 @@ def digit_recognition(padded_num):
     #print padded_num.mean()
     if padded_num.mean() >= one_threshold:
         return 1
-    #thresh_value, padded_num = cv2.threshold(padded_num, 20, 255, cv2.THRESH_BINARY)
+    thresh_value, padded_num = cv2.threshold(padded_num, 240, 255, cv2.THRESH_BINARY)
     u_x, u_y, width, height = cv2.boundingRect(padded_num)
     if width < 8: #too slim
         return 1
     #print u_x, u_y, width, height
     on = [0 for i in range(7)]
-    tube_width, tube_height = int(width * 0.25), int(height * 0.18)
+    #tube_width, tube_height = int(width * 0.25), int(height * 0.18)
+    #tube_width = int((width * 0.25 + height * 0.18) / 2)
+    tube_width = 2
+    tube_height = tube_width
     bias = int(height * 0.08)
     segments = [
-        ((u_x + bias, u_y + tube_height), (u_x + tube_width, u_y + height // 2 - tube_height)),	# top-left
-        ((u_x + bias, u_y + height // 2 + bias), (u_x + tube_width, u_y + height - tube_width - 1)),	# bottom-left
-        ((u_x + width - tube_width - bias, u_y + tube_height), (u_x + width, u_y + height // 2 - tube_height)),	# top-right
-        ((u_x + width - tube_width - bias, u_y + height // 2 + bias), (u_x + width, u_y + height - tube_width - 1)),	# bottom-right
-        ((u_x + tube_width, u_y + bias), (u_x + width - tube_width - bias, u_y + tube_height)),	# top
+        ((u_x, u_y + tube_height), (u_x + tube_width, u_y + height // 2 - tube_height)),	# top-left
+        ((u_x, u_y + height // 2 + bias), (u_x + tube_width, u_y + height - tube_width - 1)),	# bottom-left
+        ((u_x + width - tube_width, u_y + tube_height), (u_x + width - bias, u_y + height // 2 - tube_height)),	# top-right
+        ((u_x + width - tube_width, u_y + height // 2 + bias), (u_x + width - bias, u_y + height - tube_width)),	# bottom-right
+        ((u_x + tube_width, u_y), (u_x + width - tube_width - 1, u_y + tube_height - bias)),	# top
         ((u_x + tube_width, u_y + (height // 2)) , (u_x + width - tube_width, u_y + (height // 2) + bias)), # center
         ((u_x + tube_width, u_y + height - tube_height), (u_x + width - tube_width, u_y + height))	# bottom
     ]
