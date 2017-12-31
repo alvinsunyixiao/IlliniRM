cd7770d8cbb0dda2d60a7f936ee46a62bc4a8950
alvin <alvinsunyixiao@gmail.com>
2017-12-31 10:41:37 +0800

    finished mac version cmakes

--- proto-pipeline/cpp-mac/cmake/FindCaffe.cmake
+++ proto-pipeline/cpp-mac/cmake/FindCaffe.cmake
@@ -2,12 +2,11 @@
 unset(Caffe_FOUND)
 
 ###Set the variable Caffe_DIR as the root of your caffe directory
-set(Caffe_DIR /home/nvidia/caffe/distribute)
+set(Caffe_DIR ${CMAKE_SOURCE_DIR}/../../caffe/distribute)
 
 
 find_path(Caffe_INCLUDE_DIRS NAMES caffe/caffe.hpp caffe/common.hpp caffe/net.hpp caffe/proto/caffe.pb.h caffe/util/io.hpp caffe/vision_layers.hpp
     HINTS ${Caffe_DIR}/include)
-
 
 
 find_library(Caffe_LIBRARIES NAMES caffe
