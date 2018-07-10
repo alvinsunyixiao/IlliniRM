fe7a936e7f0ce363be6b897b6572709db6bc7f92
Roger Q <twofs0.5@gmail.com>
2018-07-10 15:55:43 +0800

    minimize tensorRT

--- roger_detection_test/fire_detect.py
+++ roger_detection_test/fire_detect.py
@@ -4,7 +4,7 @@ import numpy as np
 import time
 from IPython import embed
 
-baseline_mean = [170, 210, 235]
+baseline_mean = [150, 210, 230]
 baseline_std = [50, 40 ,25]
 
 def naive_test(query, baseline, threshold):
@@ -24,8 +24,8 @@ def get_contour_stats(single_contour, img):
     my_std = []
     for channel in channels:
         _mean, _stddev = cv2.meanStdDev(channel, mask = mask)
-        my_mean.append(_mean[0, 0])
-        my_std.append(_stddev[0, 0])
+        my_mean.append(int(_mean[0, 0]))
+        my_std.append(int(_stddev[0, 0]))
     return (my_mean, my_std)
 
 #def stupid_put_text(img, text, point_x, point_y):
@@ -33,6 +33,7 @@ def get_contour_stats(single_contour, img):
 cap = cv2.VideoCapture('./fire.mp4')
 while cap.isOpened():
     ret, img = cap.read()
+    img = cv2.resize(img, (640, 360))
     if not ret: break
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #gray = cv2.GaussianBlur(gray, (7, 7), 0)
@@ -51,23 +52,30 @@ while cap.isOpened():
     '''
     for ctr in pre_contours:
         area_size = cv2.contourArea(ctr)
-        if area_size > 625 and area_size < 4000: #25 * 25
+        if area_size > 400 and area_size < 4000: #25 * 25
             contours.append(ctr)
+    print(len(contours))
     post_contours = []
+    stats = []
+
+    mask_debug = np.zeros(gray.shape, dtype = "uint8")
+    for ctr in contours:
+        cv2.drawContours(mask_debug, [ctr], -1, 255, -1)
+    cv2.imshow("debug", mask_debug)
+    cv2.waitKey(1)
+
     for ctr in contours:
         cur_mean, cur_std = get_contour_stats(ctr, img)
         if naive_test(cur_mean, baseline_mean, 55) and naive_test(cur_std, baseline_std, 20):
             post_contours.append(ctr)
-    '''
             stats.append((ctr[0][0], str(cur_mean), str(cur_std)))
-        for stat in stats:
-            cv2.putText(img, stat[2],
-                        (int(stat[0][0]), int(stat[0][1])),
-                        cv2.FONT_HERSHEY_SIMPLEX,
-                        0.4,
-                       (0,255,255),
-                       2,cv2.LINE_AA)
-    '''
+    for stat in stats:
+        cv2.putText(img, stat[2],
+                    (int(stat[0][0]), int(stat[0][1])),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.4,
+                   (0,255,255),
+                   2,cv2.LINE_AA)
     cv2.drawContours(img, post_contours, -1, (255, 0, 0), 3)
     cv2.imshow("test", img)
     cv2.waitKey(1)
